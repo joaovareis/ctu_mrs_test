243554916d0dd3bb62ba0eed7cbdd9afb2a66905
joaovareis <joaoreis@usp.br>
2026-05-26 21:09:16 +0000

    Takeoff e flying funcionando no launch-demo

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -12,8 +12,9 @@ from launch_ros.descriptions import ComposableNode
 def generate_launch_description():
     ld = LaunchDescription()
 
-    set_uav_type = SetEnvironmentVariable(name='UAV_TYPE', value='x500')
+    set_uav_type = SetEnvironmentVariable(name='UAV_TYPE', value='f450')
     set_run_type = SetEnvironmentVariable(name='RUN_TYPE', value='simulation')
+    set_zenoh_config = SetEnvironmentVariable(name='ZENOH_CONFIG', value='/root/ros2_ws/zenoh_config.json')
 
     #my_autostart_config = PathJoinSubstitution([FindPackageShare('imav_26'), 'config', 'automatic_start.yaml'])
     my_world_config     = PathJoinSubstitution([FindPackageShare('imav_26'), 'config', 'world_config.yaml'])
@@ -23,7 +24,7 @@ def generate_launch_description():
     my_platform_config  = PathJoinSubstitution([FindPackageShare('imav_26'), 'config', 'x500.yaml'])
 
     zenoh_router = ExecuteProcess(
-        cmd=['ros2', 'run', 'rmw_zenoh_cpp', 'rmw_zenohd'],
+        cmd=['ros2', 'run', 'rmw_zenoh_cpp', 'rmw_zenohd', '--config', '/root/ros2_ws/zenoh_config.json'],
         output='screen'
     )
     
@@ -85,25 +86,58 @@ def generate_launch_description():
     )
 
     mrs_autostart_container = ComposableNodeContainer(
-    name='automatic_start_container',
-    namespace='uav1',
-    package='rclcpp_components',
-    executable='component_container',
-    composable_node_descriptions=[
-        ComposableNode(
-            package='mrs_uav_autostart',
-            plugin='mrs_uav_autostart::automatic_start::AutomaticStart',
-            name='automatic_start',
-            namespace='uav1',
-            parameters=[
-                PathJoinSubstitution([FindPackageShare('mrs_uav_autostart'), 'config', 'public', 'automatic_start.yaml']),
-                PathJoinSubstitution([FindPackageShare('mrs_uav_autostart'), 'config', 'private', 'automatic_start.yaml']),
-                PathJoinSubstitution([FindPackageShare('imav_26'), 'config', 'automatic_start.yaml']),
-                {'use_sim_time': True}
-            ]
-        )
-    ],
-    output='screen',
+        name='automatic_start_container',
+        namespace='uav1',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='mrs_uav_autostart',
+                plugin='mrs_uav_autostart::automatic_start::AutomaticStart',
+                name='automatic_start',
+                namespace='uav1',
+                parameters=[
+                    PathJoinSubstitution([FindPackageShare('mrs_uav_autostart'), 'config', 'public', 'automatic_start.yaml']),
+                    PathJoinSubstitution([FindPackageShare('mrs_uav_autostart'), 'config', 'private', 'automatic_start.yaml']),
+                    PathJoinSubstitution([FindPackageShare('imav_26'), 'config', 'automatic_start.yaml']),
+                    {
+                        'use_sim_time': True,
+                        'uav_name': 'uav1',
+                        'simulation': True,
+                        'custom_config': PathJoinSubstitution([FindPackageShare('imav_26'), 'config', 'automatic_start.yaml']),
+                        'config_private': PathJoinSubstitution([FindPackageShare('mrs_uav_autostart'), 'config', 'private', 'automatic_start.yaml']),
+                        'config_public': PathJoinSubstitution([FindPackageShare('mrs_uav_autostart'), 'config', 'public', 'automatic_start.yaml'])
+                    }
+                ]
+            )
+        ],
+        output='screen',
+    )
+
+    # Automated Flight Sequence Actions
+
+    zenoh_env = {'RMW_IMPLEMENTATION': 'rmw_zenoh_cpp'}
+    
+    arm_drone = ExecuteProcess(
+        cmd=['ros2', 'service', 'call', '/uav1/hw_api/arming', 'std_srvs/srv/SetBool', '{data: true}'],
+        additional_env=zenoh_env,
+        output='screen'
+    )
+    
+    activate_midair = ExecuteProcess(
+        cmd=['ros2', 'service', 'call', '/uav1/uav_manager/midair_activation', 'std_srvs/srv/Trigger'],
+        output='screen'
+    )
+
+    #takeoff_drone = ExecuteProcess(
+    #    cmd=['ros2', 'service', 'call', '/uav1/uav_manager/takeoff', 'std_srvs/srv/Trigger'],
+    #    output='screen'
+    #)
+
+    takeoff_drone = ExecuteProcess(
+        cmd=['ros2', 'service', 'call', '/uav1/control_manager/goto_fcu', 'mrs_msgs/srv/Vec4', '{goal: [0.0, 0.0, 2.5, 0.0]}'],
+        additional_env=zenoh_env,
+        output='screen'
     )
 
     delay_sim = TimerAction(
@@ -131,15 +165,23 @@ def generate_launch_description():
         actions=[mrs_autostart_container]
     )
 
-    
+    delay_arm = TimerAction(period=65.0, actions=[arm_drone])
+    delay_midair = TimerAction(period=70.0, actions=[activate_midair])
+    delay_takeoff = TimerAction(period=80.0, actions=[takeoff_drone])
+
+    ld.add_action(set_zenoh_config)
     ld.add_action(set_uav_type)
     ld.add_action(set_run_type)
     ld.add_action(zenoh_router)
-    ld.add_action(mrs_simulation)
+    ld.add_action(delay_sim)
     ld.add_action(delay_spawn)
     ld.add_action(delay_hw_api)
     ld.add_action(delay_mrs_core)
-    ld.add_action(delay_autostart)
+    #ld.add_action(delay_autostart)
+
+    ld.add_action(delay_arm)
+    ld.add_action(delay_midair)
+    ld.add_action(delay_takeoff)
 
     return ld
 
